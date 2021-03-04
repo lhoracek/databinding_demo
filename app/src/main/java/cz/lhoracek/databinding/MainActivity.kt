113cb3ca3d482cdd93db09e911991dd5310a938e
Luboš Horáček <mail@lhoracek.cz>
2021-03-04 13:28:46 +0100

    Fix

--- app/src/main/java/cz/lhoracek/databinding/MainActivity.kt
+++ app/src/main/java/cz/lhoracek/databinding/MainActivity.kt
@@ -21,7 +21,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var viewModel: ActivityViewModel by viewModel()
+    private val viewModel: ActivityViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
